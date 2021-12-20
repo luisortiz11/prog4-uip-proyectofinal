acc5ea34ee4255c294a44dbefbdd713d21b23de3
luis ortiz <11ortizluis@gmail.com>
2021-12-20 18:58:01 -0500

    mail work

--- final.py
+++ final.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms.fields.html5 import DateField
 from wtforms.fields.simple import TextField
 from wtforms import validators, SubmitField
+from flask_mail import Mail, Message
 import pymongo 
 import datetime
 
@@ -10,9 +11,18 @@ import datetime
 
 app = Flask(__name__)
 
+
 app.config['SECRET_KEY'] = "123*"
 app.config['MONGO_URI']= 'mongodb://localhost:27017/final'
 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'smiledr507@gmail.com'
+app.config['MAIL_PASSWORD'] = 'xHnfXYUDfNNsD8M'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+
+mail = Mail(app)
 class InfoForm(FlaskForm):
     nombre = TextField('Nombre:', validators=(validators.DataRequired(),))
     apellido = TextField('Apellido:', validators=(validators.DataRequired(),))
@@ -56,6 +66,13 @@ def response():
         id += 1 
     
     respuesta = "Hola {} {}, se ha registrado la cita satisfactoriamente para el dia {}, te enviaremos un correo de confirmacion a {}. Posteriormente te llamaremos a {} para informarte la disponibilidad durante el día.".format(citas[id-1]['nombre'], citas[id-1]['apellido'], citas[id-1]['fecha'], citas[id-1]['correo'],citas[id-1]['telefono'] )
+    msg = Message('Cita para Odontología', sender ='smiledr507@gmail.com', recipients = citas[id-1]['correo'].split())
+    msg.body = "Confirmación de cita"
+    mail.send(msg)
+
+    msg2 = Message('Cita para Odontología', sender ='smiledr507@gmail.com', recipients =  'smiledr507@gmail.com'.split())
+    msg2.body = "Fecha de cita {} Llamar a {} {} al {} para confimar hora de la cita".format(citas[id-1]['fecha'], citas[id-1]['nombre'], citas[id-1]['apellido'], citas[id-1]['telefono'] )
+    mail.send(msg2)
     return render_template('response.html', citas = respuesta)
 
 @app.route('/citas' , methods=[ 'GET','POST'])
@@ -63,7 +80,7 @@ def qery():
     form = Peticion()
     if form.validate_on_submit():
         session['correobusca'] = form.correo.data
-        
+    
         return redirect('/resultado')
     return render_template('busca.html', form=form)
 
@@ -73,6 +90,14 @@ def busca():
     citas = col.find_one({"correo": correo})
     respuesta = "Hola {} {}, la cita es el dia {}, te enviaremos un correo de confirmacion a {}. Posteriormente te llamaremos a {} para informarte la disponibilidad durante el día.".format(citas['nombre'], citas['apellido'], citas['fecha'], citas['correo'],citas['telefono'] )
     
+    msg = Message('Cita para Odontología', sender =   'smiledr507@gmail.com', recipients = correo.split())
+    msg.body = "Confirmación de cita"
+    mail.send(msg)
+
+    msg2 = Message('Cita para Odontología', sender =   'smiledr507@gmail.com', recipients =  'smiledr507@gmail.com'.split())
+    msg2.body = "Fecha de cita {} Llamar a {} {} al {} para confimar hora de la cita".format(citas['fecha'], citas['nombre'], citas['apellido'], citas['telefono'] )
+    mail.send(msg2)
+
     return render_template('response.html', citas = respuesta)
 
 
