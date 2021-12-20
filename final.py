de9cb1146971ee6cadc3043c5dd3aaae13486ea2
luis ortiz <11ortizluis@gmail.com>
2021-12-20 18:30:00 -0500

    busca

--- final.py
+++ final.py
@@ -20,6 +20,11 @@ class InfoForm(FlaskForm):
     correo = TextField('Correo:', validators=(validators.DataRequired(),))
     startdate = DateField('Fecha:', format='%Y-%m-%d', validators=(validators.DataRequired(),))
     submit = SubmitField('Enviar')
+    
+class Peticion(FlaskForm):
+    correo = TextField('Correo:', validators=(validators.DataRequired(),))
+    submit = SubmitField('Enviar')
+
 
 # Establece conexion con servidor MongoDB
 con = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -49,7 +54,26 @@ def response():
         citas = {}
         citas[id] = el
         id += 1 
-    return render_template('response.html', citas = list(citas.values())[-1])
+    
+    respuesta = "Hola {} {}, se ha registrado la cita satisfactoriamente para el dia {}, te enviaremos un correo de confirmacion a {}. Posteriormente te llamaremos a {} para informarte la disponibilidad durante el día.".format(citas[id-1]['nombre'], citas[id-1]['apellido'], citas[id-1]['fecha'], citas[id-1]['correo'],citas[id-1]['telefono'] )
+    return render_template('response.html', citas = respuesta)
+
+@app.route('/citas' , methods=[ 'GET','POST'])
+def qery():
+    form = Peticion()
+    if form.validate_on_submit():
+        session['correobusca'] = form.correo.data
+        
+        return redirect('/resultado')
+    return render_template('busca.html', form=form)
+
+@app.route('/resultado' , methods=[ 'GET','POST'])
+def busca():
+    correo = session.get('correobusca')
+    citas = col.find_one({"correo": correo})
+    respuesta = "Hola {} {}, la cita es el dia {}, te enviaremos un correo de confirmacion a {}. Posteriormente te llamaremos a {} para informarte la disponibilidad durante el día.".format(citas['nombre'], citas['apellido'], citas['fecha'], citas['correo'],citas['telefono'] )
+    
+    return render_template('response.html', citas = respuesta)
 
 
 if __name__ == '__main__':
